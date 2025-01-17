b9c51fdc3da8a8d457172212d8f98262f4923680
dvinnikov <samui2006@ukr.net>
2025-01-17 20:20:50 +0200

    Add CategoryController

---
+++ LibraryInventory/Controllers/CategoryController.cs
@@ -0,0 +1,56 @@
+﻿using LibraryInventory.Data;
+using LibraryInventory.Models;
+using System.Web.Http;
+
+namespace LibraryInventory.Controllers
+{
+    [RoutePrefix("api/categories")]
+    public class CategoriesController : ApiController
+    {
+        private readonly IRepository<Category> _repository;
+
+        public CategoriesController(IRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult GetCategories()
+        {
+            var categories = _repository.GetAll();
+            return Ok(categories);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IHttpActionResult AddCategory([FromBody] Category category)
+        {
+            if (category == null)
+                return BadRequest("Category cannot be null.");
+
+            _repository.Add(category);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IHttpActionResult UpdateCategory(int id, [FromBody] Category category)
+        {
+            if (category == null || id != category.Id)
+                return BadRequest("Invalid category data.");
+
+            _repository.Update(category);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IHttpActionResult DeleteCategory(int id)
+        {
+            _repository.Delete(id);
+            return Ok();
+        }
+    }
+
+}

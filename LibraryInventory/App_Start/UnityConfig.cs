a06680834ff75fee9ae6b1a77b45cf5bce35fc08
dvinnikov <samui2006@ukr.net>
2025-01-17 17:58:26 +0200

    Set up dependency injection with Unity Container add Unity web api package

---
+++ LibraryInventory/App_Start/UnityConfig.cs
@@ -0,0 +1,21 @@
+﻿using LibraryInventory.Data;
+using LibraryInventory.Models;
+using System.Web.Http;
+using Unity;
+using Unity.WebApi;
+
+namespace LibraryInventory.App_Start
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            var container = new UnityContainer();
+
+            container.RegisterType<IRepository<Book>, Repository<Book>>();
+            container.RegisterType<IRepository<Category>, Repository<Category>>();
+
+            GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
+        }
+    }
+}
